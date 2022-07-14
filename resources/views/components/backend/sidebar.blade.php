d1cb76bf3cf26dded565624503922981198810b1
Bijay Subba Limbu <bijaysubbalimbu@gmail.com>
2022-07-14 07:36:20 +0545

    Backend Format Created

--- resources/views/components/backend/sidebar.blade.php
+++ resources/views/components/backend/sidebar.blade.php
@@ -18,9 +18,7 @@
                             class="hide-menu">Products</span></a></li>
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                         href="grid.html" aria-expanded="false"><i class="mdi mdi-blur-linear"></i><span
-                            class="hide-menu">Orders</span></a></li><li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                        href="grid.html" aria-expanded="false"><i class="mdi mdi-blur-linear"></i><span
-                            class="hide-menu">Orders</span></a></li>
+                            class="hide-menu">Orders</span></a></li><li class="sidebar-item">
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                         href="grid.html" aria-expanded="false"><i class="mdi mdi-blur-linear"></i><span
                             class="hide-menu">Post</span></a></li>
